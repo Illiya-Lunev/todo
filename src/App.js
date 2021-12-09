9eeda5bb130e58dc563b21d11df1c0f1749c3f88
Iliya Lunev <lunevig90@gmail.com>
2021-12-09 18:23:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export default class App extends Component {
   };
 
   render() {
-    const { filter, todos } = this.state;
+    const { filter } = this.state;
 
     const visibleTodos = this.getVisibleTodos();
     return (
--- src/components/Todos/TodoEditor/TodoEditor.js
+++ src/components/Todos/TodoEditor/TodoEditor.js
@@ -25,7 +25,7 @@ export default class TodoForm extends Component {
     const { message } = this.state;
     return (
       <form className={'todo-editor__form'} onSubmit={this.handleSubmit}>
-        <label htmlFor={this.inputId} htmlFor={this.inputId}>
+        <label htmlFor={this.inputId}>
           Добавить
           <input
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
